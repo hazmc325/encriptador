deec5334567ab42f7611cd9d6797db74f2c3fc4b
Haziel <hazmc325@gmail.com>
2023-03-01 16:51:02 -0600

    Primera versión del Encriptador de textos mejorado para que acepte acentos

---
+++ script.js
@@ -0,0 +1,56 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const copia = document.querySelector(".copiar");
+copia.style.display = "none"
+
+//Laves de encriptacion
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+function btnEncriptar() {
+	const textoEncriptado = encriptar(textArea.value);
+	mensaje.value = textoEncriptado;
+	textArea.value = "";
+	mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada){
+	let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"],
+		["á","zexj"], ["é","fpce"], ["í","oie"], ["ó","kyyq"], ["ú","plzg"]];
+	stringEncriptada = stringEncriptada.toLowerCase();
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+		if (stringEncriptada.includes(matrizCodigo[i][0])) {
+			stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+		}
+	}
+	return stringEncriptada;
+}
+
+function btnDesencriptar() {
+	const textoDesencriptado = desencriptar(textArea.value);
+	mensaje.value = textoDesencriptado;
+	textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada){
+	let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"],
+		["á","zexj"], ["é","fpce"], ["í","oie"], ["ó","kyyq"], ["ú","plzg"]];
+	stringDesencriptada = stringDesencriptada.toLowerCase();
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+		if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+			stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+		}
+	}
+	return stringDesencriptada;
+}
+
+function copiar() {
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+}
